46834ae531471945fb8b84e976e17cf937347a9b
leyuskckiran1510 <kdhakal1510@gmail.com>
2025-01-18 20:02:03 +0545

    feat: add collion, marquee and controll setting

--- src/game.c
+++ src/game.c
@@ -1,4 +1,9 @@
 #include "../raylib/include/raylib.h"
+#include <math.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #define CreateWindow(...) InitWindow(__VA_ARGS__)
 #define SIZE(x,y)  (x),(y)
@@ -30,12 +35,33 @@ typedef struct _Rectangle{
     Vector2 pos;
     int width;
     int height;
-} _Rectangle;
+} _Rectangle,Box;
 
 typedef struct _Line{
     Vector2 pos;
     Vector2 pos2;
 } _Line;
+
+
+typedef enum MarqueeDirection{
+    MRQ_NONE = 0,
+
+    LEFT_TO_RIGHT,
+    LEFT_TO_RIGHT_WRAP,
+
+    RIGHT_TO_LEFT,
+    RIGHT_TO_LEFT_WRAP,
+
+
+    TOP_TO_BOTTOM,
+    TOP_TO_BOTTOM_WRAP,
+
+
+    BOTTOM_TO_TOP,
+    BOTTOM_TO_TOP_WRAP,
+    
+}MarqueeDirection;
+
 
 typedef struct Object{
     ObjectType type;
@@ -47,6 +73,10 @@ typedef struct Object{
     Color color;
     Vector2 velocity;
     Controller controller;
+    MarqueeDirection marquee;
+    int marquee_speed;
+    int follows_gravity;
+    int is_opaque;
 } Object;
 
 
@@ -56,7 +86,6 @@ typedef struct Objects{
 } Objects;
 
 typedef struct Physics{
-    int gravity;
     int walls;
     float wall_damping;
     float jump_strength;
@@ -66,6 +95,8 @@ typedef struct Physics{
 
 static Objects objects={.count=0};
 static Physics physics_config = {0,.wall_damping=0.5714278,.jump_strength=6.9,.move=1.5};
+static int game_over = 0;
+static int score = 0;
 
 int AddBall(Vector2 pos,int radius,Color color){
     int index = objects.count++;
@@ -105,12 +136,153 @@ void RenderObjects(){
 #define ObjCol(index)  objects.items[(index)].color
 #define ObjRadius(index)  objects.items[(index)].shape.radius
 
+
+void handel_controllers(Object *object){
+    switch (object->controller) {
+            case CNTLR_NONE:break;
+            case CNTLR_JUMPER:{
+                if (IsKeyPressed(KEY_SPACE)) {
+                    object->velocity.y = -physics_config.jump_strength;
+                }
+                break;
+            }
+            case CNTLR_2AXIS:{
+                if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) {
+                    object->velocity.y -= physics_config.move;
+                }
+                
+                if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) {
+                    object->velocity.y += physics_config.move;
+                }
+                
+                if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
+                    object->velocity.x -= physics_config.move;
+                }
+                
+                if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
+                    object->velocity.x += physics_config.move;
+                }
+
+                if (!IsKeyDown(KEY_A) && !IsKeyDown(KEY_D) && 
+                    !IsKeyDown(KEY_LEFT) && !IsKeyDown(KEY_RIGHT)) {
+                    object->velocity.x *= 0.9f;
+                }
+                if (!IsKeyDown(KEY_W) && !IsKeyDown(KEY_S) && 
+                    !IsKeyDown(KEY_UP) && !IsKeyDown(KEY_DOWN)) {
+                    object->velocity.y *= 0.9f;
+                }
+                break;
+            }
+        }
+}
+
+
+int get_object_width(Object object){
+    switch (object.type) {
+    case BALL:{
+        return object.shape.radius;
+    }
+    case BOX:
+        return object.rec.width;
+    case LINE:{
+        int width = fabs(object.line.pos.x-object.line.pos2.x)+fabs(object.line.pos.y-object.line.pos2.y);
+        return width;
+    }
+    }
+}
+
+int get_object_height(Object object){
+    switch (object.type) {
+        case BALL:{
+            return object.shape.radius;
+        }
+        case BOX:
+            return object.rec.height;
+        case LINE:{
+            return 1;
+        }
+        }
+}
+
+
+void handel_marquee(Object *object) {
+
+    int width = get_object_width(*object);
+    int height = get_object_height(*object);
+    int marquee_speed = object->marquee_speed;
+    switch (object->marquee) {
+        case MRQ_NONE:
+            return;
+        case LEFT_TO_RIGHT:
+            object->shape.pos.x += physics_config.move;
+            if (object->shape.pos.x > GetScreenWidth() - width) {
+                object->shape.pos.x = GetScreenWidth() - width; // Stop at the right edge
+            }
+            break;
+
+        case LEFT_TO_RIGHT_WRAP:
+            object->shape.pos.x += marquee_speed;
+            if (object->shape.pos.x > GetScreenWidth()) {
+                object->shape.pos.x = -width; // Wrap to the left
+            }
+            break;
+
+        case RIGHT_TO_LEFT:
+            object->shape.pos.x -= marquee_speed;
+            if (object->shape.pos.x < 0) {
+                object->shape.pos.x = 0; // Stop at the left edge
+            }
+            break;
+
+        case RIGHT_TO_LEFT_WRAP:
+            object->shape.pos.x -= marquee_speed;
+            if (object->shape.pos.x < -width) {
+                object->shape.pos.x = GetScreenWidth(); // Wrap to the right
+                score++;
+            }
+            break;
+
+        case TOP_TO_BOTTOM:
+            object->shape.pos.y += marquee_speed;
+            if (object->shape.pos.y > GetScreenHeight() - height) {
+                object->shape.pos.y = GetScreenHeight() - height; // Stop at the bottom
+            }
+            break;
+
+        case TOP_TO_BOTTOM_WRAP:
+            object->shape.pos.y += marquee_speed;
+            if (object->shape.pos.y > GetScreenHeight()) {
+                object->shape.pos.y = - height; // Wrap to the top
+            }
+            break;
+
+        case BOTTOM_TO_TOP:
+            object->shape.pos.y -= marquee_speed;
+            if (object->shape.pos.y < 0) {
+                object->shape.pos.y = 0; // Stop at the top edge
+            }
+            break;
+
+        case BOTTOM_TO_TOP_WRAP:
+            object->shape.pos.y -= marquee_speed;
+            if (object->shape.pos.y < - height) {
+                object->shape.pos.y = GetScreenHeight(); // Wrap to the bottom
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+
 void ApplyPhysics(){
+    if(game_over) return;
     for(int i=0;i<objects.count;i++){
         ObjX(i) += objects.items[i].velocity.x;     
         ObjY(i) += objects.items[i].velocity.y;  
 
-        if(physics_config.gravity){
+        if(objects.items[i].follows_gravity){
             objects.items[i].velocity.y += GRAVITY; 
         }
         if(physics_config.walls){
@@ -124,46 +296,126 @@ void ApplyPhysics(){
                 objects.items[i].velocity.y *=(physics_config.wall_damping);
             }
         }
-        switch (objects.items[i].controller) {
-            case CNTLR_NONE:break;
-            case CNTLR_JUMPER:{
-                if (IsKeyPressed(KEY_SPACE)) {
-                    objects.items[i].velocity.y = -physics_config.jump_strength;
-                }
-                break;
-            }
-            case CNTLR_2AXIS:{
-                if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) {
-                    objects.items[i].velocity.y -= physics_config.move;
-                }
-                
-                if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) {
-                    objects.items[i].velocity.y += physics_config.move;
-                }
-                
-                if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
-                    objects.items[i].velocity.x -= physics_config.move;
-                }
-                
-                if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
-                    objects.items[i].velocity.x += physics_config.move;
-                }
-
-                if (!IsKeyDown(KEY_A) && !IsKeyDown(KEY_D) && 
-                    !IsKeyDown(KEY_LEFT) && !IsKeyDown(KEY_RIGHT)) {
-                    objects.items[i].velocity.x *= 0.9f;
-                }
-                if (!IsKeyDown(KEY_W) && !IsKeyDown(KEY_S) && 
-                    !IsKeyDown(KEY_UP) && !IsKeyDown(KEY_DOWN)) {
-                    objects.items[i].velocity.y *= 0.9f;
-                }
-                break;
-            }
-        }
+        handel_controllers(&objects.items[i]);
+        handel_marquee(&objects.items[i]);
     }
 }
 
 #define StartDrawing() BeginDrawing();ApplyPhysics();RenderObjects();
-#define AddGravity() physics_config.gravity=1;
+#define AddGravity(object_id)  objects.items[object_id].follows_gravity = 1;
+#define AddCollision(object_id)  objects.items[object_id].is_opaque = 1;
 #define AddWalls()   physics_config.walls=1;
 #define AddControlls(object_id,control_type) objects.items[object_id].controller=control_type;
+
+
+int AddMovingBox(Box rec ,MarqueeDirection dircetion,Color color){
+    int index = objects.count++;
+    objects.items[index].type = BOX;
+    objects.items[index].rec = rec;
+    objects.items[index].velocity = (Vector2){.x=0,.y=0};
+    objects.items[index].color = color;
+    objects.items[index].marquee = dircetion;
+    objects.items[index].marquee_speed = 5;
+    return index;
+}
+
+
+
+
+int CheckCollisions(){
+    for (int i=0;i<objects.count;i++){
+        if(!objects.items[i].is_opaque) continue;
+        for (int j=0;j<objects.count;j++){
+            if(objects.items[i].type==BALL  && objects.items[j].type==BOX){
+                if(CheckCollisionCircleRec(objects.items[i].shape.pos,objects.items[i].shape.radius,(Rectangle){
+                    .x = objects.items[j].rec.pos.x,
+                    .y = objects.items[j].rec.pos.y,
+                    .height=objects.items[j].rec.height,
+                    .width=objects.items[j].rec.width,
+                })){
+                    objects.items[j].color = BLUE;
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+typedef enum {
+    CENTER=0,
+    
+    TOP_LEFT,
+    TOP_RIGHT,
+    
+    BOTTOM_LEFT,
+    BOTTOM_RIGHT,
+
+} TEXT_POS;
+
+void PutText(char * text,TEXT_POS pos, Color color){
+    int x,y;
+    switch(pos){
+
+    case CENTER:{
+        x= GetScreenWidth()/2;
+        y= GetScreenHeight()/2;
+        break;
+    }
+    case TOP_LEFT:{
+        x= GetScreenWidth()/2;
+        y= GetScreenHeight()/2;
+        break;
+    }
+    case TOP_RIGHT:{
+        x= GetScreenWidth()/2;
+        y= GetScreenHeight()/2;
+        break;
+    }
+    case BOTTOM_LEFT:{
+        x= GetScreenWidth()/2;
+        y= GetScreenHeight()/2;
+        break;
+    }
+    case BOTTOM_RIGHT:{
+        x= GetScreenWidth()/2;
+        y= GetScreenHeight()/2;
+        break;
+    }
+      break;
+    }
+    x-=MeasureText(text,24)/2;
+    DrawText(text,x,y,24,color);
+}
+
+
+
+void GenerateBoxes(int count, float minGap, float maxGap) {
+      srand(time(NULL));
+      float boxWidth = 100;
+      float minHeight = 50;
+      float maxHeight = GetScreenHeight() - minGap - minHeight;
+      float xOffset = GetScreenWidth();
+      float gap = 0;
+      for (int i = 0; i < count; i++) {
+            gap = minGap + rand() % (int)(maxGap - minGap + 1);
+            float topHeight = minHeight + rand() % (int)(maxHeight - minHeight + 1);
+            float bottomHeight = GetScreenHeight() - topHeight - gap;
+            AddMovingBox(
+                (Box){
+                    .pos = {.x = xOffset, .y = 0},
+                    .height = topHeight,
+                    .width = boxWidth,
+                },
+                RIGHT_TO_LEFT_WRAP, GREEN);
+
+            AddMovingBox(
+                (Box){
+                    .pos = {.x = xOffset, .y = GetScreenHeight() - bottomHeight},
+                    .height = bottomHeight,
+                    .width = boxWidth,
+                },
+                RIGHT_TO_LEFT_WRAP, GREEN);
+            xOffset += boxWidth + 200;
+      }
+}
--- src/main.c
+++ src/main.c
@@ -5,10 +5,20 @@ int main(){
     SetGameFPS(60);
     int object_id = AddBall((Vector2){.x=200,.y=240},10,RED);  
     AddControlls(object_id,CNTLR_JUMPER);
-    AddGravity();
+    AddCollision(object_id);
+    AddGravity(object_id);
     AddWalls();
+    GenerateBoxes(3,100,200);
+
     while(WindowNotClosed()){
         StartDrawing();
+        if(CheckCollisions()){
+            PutText("Game Over",CENTER,RED);
+            game_over=1;
+        }
+        if(game_over && IsKeyPressed(KEY_ENTER)){
+            break;
+        }
         ClearBackground(BG_COLOR);
         EndDrawing();
     }
